a8f0ff176c62f9f7b31fb52d669a9947d18657e9
fmozo <andres.031felipe@hotmail.com>
2022-09-06 09:53:20 -0500

    Ajuste Ejercicio Final

--- customer-service/src/main/java/com/jcodepoint/customerservice/CustomerController.java
+++ customer-service/src/main/java/com/jcodepoint/customerservice/CustomerController.java
@@ -5,19 +5,26 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
 @RestController
 public class CustomerController {
+
+    Logger logger = LoggerFactory.getLogger(CustomerController.class);
 
 	private static Map<Integer, Customer> customers = new HashMap<>(); 	
 	
     @PostMapping("/customer")  
     public String addCustomer(@RequestBody Customer customer) { 
         if (customer.getId()<=0){
+            logger.error("El id no es valido");
             return "El id no es valido";
         }
         customers.put(customer.getId(), customer); 
@@ -27,7 +34,7 @@ public class CustomerController {
     @GetMapping("/customer")  
     public List<Customer> getCustomers() { 
         if(customers.values().isEmpty()){
-           System.out.print("No hay registros");
+            logger.info("No hay registros con ese id");
         }
     
          return new ArrayList<Customer>(customers.values());
